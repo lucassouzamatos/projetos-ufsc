16253b56fdae2c14cc7fa9d70f4cbcf86c913b4e
lucassouzamatos <lucas.souza69@hotmail.com>
2019-05-15 02:00:32 -0300

    normalização de documentos

--- mining/xls.py
+++ mining/xls.py
@@ -1,5 +1,34 @@
-import pandas as pd
+import os
+import xlrd
+
+from pymongo import MongoClient
+from __env__ import Env
 
 class Xls:
     def __init__(self):
-        print(pd.read_excel('./docs/projetos_.xls'))
+        data = xlrd.open_workbook('./docs/projetos_.xls')
+        sheet = data.sheet_by_index(0)
+
+        client = MongoClient(Env('MONGODB_URL').value)
+
+        for item in self.normalizer(sheet):
+            print('Inserted => ', client.ufsc.projetos.insert_one(item).inserted_id)
+
+    def normalizer(self, sheet):
+        for i in range(sheet.nrows):
+            projeto = {
+                "start":       self.value(sheet.row(i), 0),        
+                "finish":      self.value(sheet.row(i), 1),        
+                "status":      self.value(sheet.row(i), 2),        
+                "title":       self.value(sheet.row(i), 3),        
+                "describe":    self.value(sheet.row(i), 4),        
+                "coordinator": self.value(sheet.row(i), 5),        
+                "contact":     self.value(sheet.row(i), 6),
+                "department":  self.value(sheet.row(i), 7),
+                "funders":     self.value(sheet.row(i), 8),
+                "total":       self.value(sheet.row(i), 9)
+            }
+            yield projeto
+
+    def value(self, row, i):
+        return row[i].value
