955e7f2a2bb24fa8dc3436f04745ac112a6ce332
lucassouzamatos <lucas.souza69@hotmail.com>
2019-05-15 13:08:29 -0300

    abstraindo db

--- mining/_database_.py
+++ mining/_database_.py
@@ -1,3 +1,9 @@
+from _env_ import Env
+from pymongo import MongoClient
+
 class Database:
     def __init__(self):
-    
+        self.client = MongoClient(Env('MONGODB_URL').value)
+
+    def insert(self, collection, item):
+        print('Inserted => ', client.ufsc[collection].insert_one(item).inserted_id)
--- mining/migrate.py
+++ mining/migrate.py
@@ -1,8 +1,7 @@
 import os
 import xlrd
 
-from pymongo import MongoClient
-from _env_ import Env
+from _database_ import Database
 from normalizer import Normalizer
 
 class Migrate:
@@ -10,10 +9,8 @@ class Migrate:
         data = xlrd.open_workbook(doc)
         sheet = data.sheet_by_index(0)
 
-        client = MongoClient(Env('MONGODB_URL').value)
-
         for item in self.normalizer(sheet):
-            print('Inserted => ', client.ufsc.projetos.insert_one(item).inserted_id)
+            Database.insert('projetos', item)
 
     def normalizer(self, sheet):
         for i in range(sheet.nrows):
