03673e73c6f146298981d84bbc8da0a442731192
lucassouzamatos <lucas.souza69@hotmail.com>
2019-05-17 00:55:56 -0300

    alteração na busca de dados por coluna

--- mining/normalizer.py
+++ mining/normalizer.py
@@ -2,16 +2,17 @@ class Normalizer:
     def get(self, university: str, sheet: str, i: str):
         if university == 'ufsc':
             return {
-                "start":       self.value(sheet.row(i), 0),        
-                "finish":      self.value(sheet.row(i), 1),        
-                "status":      self.value(sheet.row(i), 2),        
-                "title":       self.value(sheet.row(i), 3),       
-                "describe":    self.value(sheet.row(i), 4),       
-                "coordinator": self.value(sheet.row(i), 5),        
-                "contact":     self.value(sheet.row(i), 6),
-                "department":  self.value(sheet.row(i), 7),
-                "funders":     self.value(sheet.row(i), 8),
-                "total":       self.value(sheet.row(i), 9)
+                "start":        self.value(sheet.row(i), 0),        
+                "finish":       self.value(sheet.row(i), 1),        
+                "status":       self.value(sheet.row(i), 2),        
+                "title":        self.value(sheet.row(i), 3),       
+                "describe":     self.value(sheet.row(i), 4),       
+                "coordinator":  self.value(sheet.row(i), 5),        
+                "contact":      self.value(sheet.row(i), 6),
+                "department":   self.value(sheet.row(i), 7),
+                "participants": self.value(sheet.row(i), 8),
+                "funders":      self.value(sheet.row(i), 9),
+                "total":        self.value(sheet.row(i), 10)
             }
 
     def value(self, row, i):
