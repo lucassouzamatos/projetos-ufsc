68eccf0b23f6b08e987649e699d31ba087b4c4aa
lucassouzamatos <lucas.souza69@hotmail.com>
2019-05-29 13:03:57 -0300

    adicionando metodo de inserir muitos registros

--- research/utils/database.py
+++ research/utils/database.py
@@ -5,5 +5,8 @@ class Database:
     def __init__(self):
         self.client = MongoClient(Env('MONGODB_URL').value)
 
-    def insert(self, collection: str, item: {}):
-        print('Inserted => ', self.client.ufsc[collection].insert_one(item).inserted_id)
+    def insert(self, collection, item: {}):
+        if type(collection) == str:
+            print('Inserted one => ', self.client.ufsc[collection].insert_one(item).inserted_id)
+        else 
+            print('Inserted many => ', self.client.ufsc[collection].insert_many(item))
