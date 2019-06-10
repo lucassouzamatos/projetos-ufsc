26e949cc62b78e50a861b993580eaa037d43ce19
lucassouzamatos <lucas.souza69@hotmail.com>
2019-06-10 12:55:08 -0300

    adicionando dados no frontend

--- app/src/pages/home/index.js
+++ app/src/pages/home/index.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 
 import { Container, Box, Header } from './styles';
+import { Chart } from "react-google-charts";
 
 export default class Home extends Component {
   render() {
     return (
         <Container>
             <Header>
-                Labore elit tempor minim in minim dolor ullamco. Id commodo pariatur dolor id labore ea exercitation nisi. Ad exercitation consequat cupidatat id excepteur est qui.
+                <span className="title">Lorem ipsum</span>
+                <span className="subtitle">Lorem ipsum</span>
             </Header>
 
-            <Box color="#040224" text="white">Aute quis mollit Lorem eu consequat tempor. Incididunt nulla qui minim pariatur ullamco. Proident esse laboris magna exercitation mollit dolor nulla. Tempor aute magna excepteur elit aliquip elit incididunt commodo ea cupidatat ex esse occaecat aute. Sint nulla culpa ex qui incididunt deserunt deserunt commodo. Velit mollit enim adipisicing ea labore excepteur id pariatur ipsum nulla duis.</Box>
-            <Box>Consequat magna dolor excepteur sit. Cillum cupidatat tempor proident pariatur. Qui nostrud non nisi consectetur ut ad irure non ut. Laboris ex ullamco minim dolor excepteur magna aliquip. Pariatur minim labore ex nulla nostrud fugiat esse excepteur non.</Box>
-            <Box color="#040224" text="white">Aute quis mollit Lorem eu consequat tempor. Incididunt nulla qui minim pariatur ullamco. Proident esse laboris magna exercitation mollit dolor nulla. Tempor aute magna excepteur elit aliquip elit incididunt commodo ea cupidatat ex esse occaecat aute. Sint nulla culpa ex qui incididunt deserunt deserunt commodo. Velit mollit enim adipisicing ea labore excepteur id pariatur ipsum nulla duis.</Box>
+            <Box className="count-box" color="#390e3d" text="white">
+              <div className="divider">
+                <p><span className="count">50.000</span> projetos em andamento</p>
+                <p><span className="count">11.525</span> relacionados a educação</p>
+              </div>
+
+              <div className="divider">
+                <p><span className="count">5.548</span> relacionados a saúde</p>
+                <p><span className="count">38.146</span> relacionados a tecnologia</p>
+              </div>
+            </Box>
+            <Box>
+              <Chart
+                chartType="ScatterChart"
+                data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+                height="400px"
+                legendToggle
+              />
+            </Box>
+            <Box color="#af3365" text="white">Aute quis mollit Lorem eu consequat tempor. Incididunt nulla qui minim pariatur ullamco. Proident esse laboris magna exercitation mollit dolor nulla. Tempor aute magna excepteur elit aliquip elit incididunt commodo ea cupidatat ex esse occaecat aute. Sint nulla culpa ex qui incididunt deserunt deserunt commodo. Velit mollit enim adipisicing ea labore excepteur id pariatur ipsum nulla duis.</Box>
             <Box>Consequat magna dolor excepteur sit. Cillum cupidatat tempor proident pariatur. Qui nostrud non nisi consectetur ut ad irure non ut. Laboris ex ullamco minim dolor excepteur magna aliquip. Pariatur minim labore ex nulla nostrud fugiat esse excepteur non.</Box>
         </Container>
     );
--- app/src/pages/home/styles.js
+++ app/src/pages/home/styles.js
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
+import header from '../../assets/header.png';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    .count-box {
+        display: flex;
+        justify-content: space-evenly;
+        
+        .count {
+            font-size: 60px;
+        }
+    }
+`;
 
 export const Box = styled.div`
     background-color: ${props => props.color ? props.color : 'white'};
@@ -9,8 +19,27 @@ export const Box = styled.div`
 `;
 
 export const Header = styled.div`
-    background:
-        linear-gradient(to bottom, rgba(26, 30, 228, 0.15), rgba(26, 30, 228, 0.5)),
-        url(-);
-    height: 300px; 
+    background-image: url(${header});
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 500px; 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 100px;
+
+    span {
+        color: #fff;
+    }
+
+    .title {
+        font-size: 48px;
+        text-shadow: 3px 3px #040224; 
+    }
+
+    .subtitle {
+        text-shadow: 2px 2px #040224; 
+    }
+
+    
 `
